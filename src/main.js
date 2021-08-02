34421b33e6670139fbabb29f3bc1a17bb11a036d
LAlex <trewstaples@gmail.com>
2021-08-02 18:49:29 +0300

    1.2 Добавит функцию для отрисовки DOM-компонентов

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = siteMainElement.querySelector('.main__control');
+
+render(siteHeaderElement, createSiteMenuTemplate(), 'beforeend');
