ac87769cfaea5797318bf298b04bc3016ed5b59b
LAlex <trewstaples@gmail.com>
2021-08-22 22:03:13 +0300

    Добавит наследование компоненту no-point

--- src/view/no-point.js
+++ src/view/no-point.js
@@ -1,25 +1,11 @@
-import { createElement } from '../utils';
+import AbstractView from './abstract.js';
 
 const createNoPointTemplate = () => '<p class="trip-events__msg">Click New Event to create your first point</p>';
 
-class NoPoint {
-  constructor() {
-    this._element = null;
-  }
-
+class NoPoint extends AbstractView {
   getTemplate() {
     return createNoPointTemplate();
   }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
-  }
 }
+
 export default NoPoint;
